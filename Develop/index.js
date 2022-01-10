1ea2aa01fee401786ac42f9c7c8bb025d69a5d3c
Cory Thompson <cory.thompson98@gmail.com>
2022-01-10 16:49:20 -0600

    Update index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,52 @@
 // TODO: Include packages needed for this application
+const { writeFile, copyFile } = require('./utils/generateMarkdown.js');
+const inquirer = require('inquirer');
+const generatePage = require('');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the name of your program?',
+            validate: nameInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a title!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'What are your installation instructions? (Required)',
+            validate: installInput => {
+                if (installInput) {
+                    return true
+                } else {
+                    console.log('An installation guide is required!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Explain how your program is used. (Required)',
+            validate: usageInput => {
+                if (usageInput) {
+                    return true
+                } else {
+                    console.log('Please explain how your program is used!');
+                    return false;
+                }
+            }
+        }
+    ])
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
