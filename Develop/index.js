442c387822fdd2060d4fe9e04fc4466501088703
Cory Thompson <cory.thompson98@gmail.com>
2022-01-16 15:35:02 -0600

    Update index.js

--- Develop/index.js
+++ Develop/index.js
@@ -10,12 +10,34 @@ const inquirer = require('inquirer');
 //     console.log('Your ReadMe is complete! Check out README.md to see the output!');
 // });
 
-inquirer
-    .prompt([
+const promptUser = () => {
+    return inquirer.prompt([
         {
             type: 'input',
             name: 'title',
-            message: 'What is the title of your project?'
-        }
-    ])
-    .then(answers => console.log(answers));
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please provide a detailed description of your project.'
+        },
+        {
+            type: 'input',
+            name: 'installion', 
+            message: 'Explain how to intall your program.'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide an explanation to use your program. Use screenshots if necessary.'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Please provide a link to your github profile.'
+        },
+    ]);
+};
+
+promptUser().then(answers => console.log(answers));
