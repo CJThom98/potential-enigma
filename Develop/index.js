c214b20767c32d983f871e42e32e4df9960e9c7a
Cory Thompson <cory.thompson98@gmail.com>
2022-01-16 15:41:40 -0600

    validation

--- Develop/index.js
+++ Develop/index.js
@@ -15,27 +15,67 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'title',
-            message: 'What is the name of your project?'
+            message: 'What is the name of your project? (Required)',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the title of your project!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'description',
-            message: 'Please provide a detailed description of your project.'
+            message: 'Please provide a detailed description of your project.',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Describe your project!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'installion', 
-            message: 'Explain how to intall your program.'
+            message: 'Explain how to intall your program.',
+            validate: installtionInput => {
+                if (installtionInput) {
+                    return true;
+                } else {
+                    console.log('Describe the installtion process');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'usage',
-            message: 'Provide an explanation to use your program. Use screenshots if necessary.'
+            message: 'Provide an explanation to use your program. Use screenshots if necessary.',
+            validate: usageInput => {
+                if (usageInput) {
+                    return true;
+                } else {
+                    console.log('Describe how the User is suppose to use your program!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'github',
-            message: 'Please provide a link to your github profile.'
+            message: 'Please provide a link to your github profile.',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Add a link to your github profile!');
+                    return false;
+                }
+            }
         },
     ]);
 };
