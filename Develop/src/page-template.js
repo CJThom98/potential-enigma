6cf098e232bb41ed7db890d8eb0a780b1a98f9cf
Cory Thompson <cory.thompson98@gmail.com>
2022-01-16 17:01:19 -0600

    Update page-template.js

--- Develop/src/page-template.js
+++ Develop/src/page-template.js
@@ -1,18 +1,67 @@
-const generatePage = (project, description, installation, usage, github) => { return`
-# ${project}
+const generateProject = projectText => {
+    if (!projectText) {
+        return "";
+    }
 
-## Description
-    ${description}
-
-## How to Install
-    ${installation}
-
-## How to Use
-    ${usage}
-
-## My GitHub Profile
-    ${github}
+    return `
+        ${project}
     `;
 };
 
-module.exports = generatePage;
+const generateDescription = descriptionText => {
+    if (!descriptionText) {
+        return "";
+    }
+
+    return `
+        ${description}
+    `;
+};
+
+const generateInstall = installText => {
+    if (!installText) {
+        return "";
+    }
+
+    return `
+        ${installation}
+    `;
+};
+
+const generateUsage = usageText => {
+    if (!usageText) {
+        return "";
+    }
+    return `
+        ${usage}
+    `;
+};
+
+const generateGithub = githubText => {
+    if (!githubText) {
+        return "";
+    }
+    return `
+        ${github}
+    `;
+};
+
+module.exports = templateData => { 
+    const { project, description, installation, usage, github } = templateData;
+    
+    return`
+# ${generateProject(project)}
+
+## Description
+    ${generateDescription(description)}
+
+## Installation
+    ${generateInstall(installation)}
+
+## Usage
+    ${generateUsage(usage)}
+
+## My GitHub Profile
+    ${generateGithub(github)}
+    `;
+};
